d0a8bb95416205697da180447fac455dfbb789e5
Benoît Zugmeyer <bzugmeyer@gmail.com>
2015-01-04 16:19:21 +0100

    Some space

--- src/applyTransforms.js
+++ src/applyTransforms.js
@@ -5,10 +5,13 @@ var hash = JSON.stringify;
 
 function applyTransforms(transforms, declarations, transformCache) {
     var property;
+
     for (property in declarations) {
         var value = declarations[property];
+
         if (property in transforms) {
             var key = property + ':' + hash(value);
+
             if (!(key in transformCache)) {
                 transformCache[key] = transforms[property](value);
                 applyTransforms(transforms, transformCache[key], transformCache);
