f76904c6caabc2f5994a47cbb25b3d911b55dbf7
Benoît Zugmeyer <bzugmeyer@gmail.com>
2015-02-18 00:57:20 +0100

    Doc script done

--- make_doc.bash
+++ make_doc.bash
@@ -1,28 +1,97 @@
 #!/bin/bash
 
 set -euo pipefail
+shopt -s globstar
 
-ROOT=$(cd "$(dirname "$0")"; pwd)
+cd "$(dirname "$0")"
 
-function relative_path () {
-    local file=$1
-    local dest=$2
+rst2html=$(
+    which rst2html 2> /dev/null ||
+    which rst2html.py 2> /dev/null)
 
-    echo $file $dest
+if [[ -z $rst2html ]]; then
+    echo 'You must have the command rst2html'
+    exit 1
+fi
+
+function generate_toc() {
+    local current_file=$1
+    local relative_path=$2
+    printf "<h1>"
+    generate_toc_entry "$current_file" "$relative_path" "doc/index.rst" "00 sans-sel"
+    printf "</h1>"
+    printf "<nav class='toc'>"
+    generate_toc_rec doc "${@}"
+    printf "</nav>"
 }
 
-mkdir -p "$ROOT/doc"
-cp "$ROOT/doc/style.css" "$ROOT/dist/doc/style.css"
-pygmentize -S friendly -f html -a .code >> "$ROOT/dist/doc/style.css"
+function generate_toc_rec() {
+    local root=$1
+    local current_file=$2
+    local relative_path=$3
 
-while IFS= read -r -d $'\0' file; do
+    printf "<ul>"
+    for file in "$root"/*; do
+        local name=${file##*/}
+        local name=${name%.*}
+
+        if [[ -d "$file" ]]; then
+            if [[ -e "$file/index.rst" ]]; then
+                printf "<li>"
+                generate_toc_entry "$current_file" "$relative_path" "$file/index.rst" "$name"
+            else
+                printf "<li>${name:3}"
+            fi
+            generate_toc_rec "$file" "$current_file" "$relative_path"
+            printf "</li>"
+        elif [[ $file =~ .*\.rst ]] && ! [[ $file =~ .*/index.rst ]]; then
+            printf "<li>"
+            generate_toc_entry "$current_file" "$relative_path" "$file" "$name"
+            printf "</li>"
+        fi
+    done
+    printf "</ul>"
+}
+
+function generate_toc_entry() {
+    local current_file=$1
+    local file=$3
+    local url=$relative_path/${file#*/}
+    local url=${url%.*}.html
+    local name=$4
+
+    local current=$([[ $file == $current_file ]] && echo ' class="current"')
+
+    printf "<a $current href='$url'>${name:3}</a>"
+}
+
+generate_file() {
+    local file="$1"
+    local directory="$(dirname "$file")"
+    local relative_path=$(dirname "$directory" | sed 's~[^/.][^/]*~..~g')
+    local stylesheet=$relative_path/style.css
+
     echo "$file"
-    mkdir -p "dist/doc/$(dirname "$file")"
-    stylesheet=$(dirname "$file" | sed 's/[^\/.][^\/]*/../g')/style.css
-    #echo $(relative_path $file doc/foo.css)
-    rst2html \
+
+    mkdir -p "dist/$directory"
+
+    $rst2html \
         --stylesheet="$stylesheet" \
         --link-stylesheet \
         --syntax-highlight=short \
-        "doc/$file" > "dist/doc/${file%.rst}.html"
-done < <(find ./doc -name '*.rst' -printf '%P\0')
+        --template="doc/template.html" \
+        --initial-header-level=2 \
+        "$file" \
+    | sed "/#TOC#/ c\\$(generate_toc "$file" "$relative_path")" \
+    > "dist/${file%.rst}.html"
+}
+
+if [[ $# -gt 0 ]]; then
+    generate_file "$1"
+else
+    mkdir -p dist/doc
+    cp doc/style.css dist/doc/style.css
+    pygmentize -S friendly -f html -a .code >> dist/doc/style.css
+
+    find doc -iname '*.rst' -print0 | xargs -0 -n1 -P0 bash $0
+fi
