947a7d625d5b3058c34237be19a7bf9d36520a76
Benoît Zugmeyer <bzugmeyer@gmail.com>
2017-06-02 11:14:02 +0200

    add a babel external helper whitelist

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,14 @@ import babel from "rollup-plugin-babel";
 
 export default {
     entry: "src/sansSel.js",
-    plugins: [ babel() ],
+    plugins: [
+        babel({
+            // Add a whitelist, else the AsyncGenerator helper is included no matter what.
+            externalHelpersWhitelist: [
+                "typeof",
+            ],
+        }),
+    ],
     format: "umd",
     moduleName: "sansSel",
 };
